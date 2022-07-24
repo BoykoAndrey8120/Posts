abd8c057633765db6914da2df91bda65527cbaab
Andrey <boyko8120@gmail.com>
2022-07-24 21:40:43 +0300

    add PostPresentation and DataFormatter

---
+++ Posts/Posts/Model/DateFormatter.swift
@@ -0,0 +1,8 @@
+//
+//  DateFormatter.swift
+//  Posts
+//
+//  Created by Andrey Boyko on 24.07.2022.
+//
+
+import Foundation
---
+++ Posts/Posts/Model/PostPresentation.swift
@@ -0,0 +1,8 @@
+//
+//  PostPresentation.swift
+//  Posts
+//
+//  Created by Andrey Boyko on 24.07.2022.
+//
+
+import Foundation
